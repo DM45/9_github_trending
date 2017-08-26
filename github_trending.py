a9771daae9c3a2ae661ae6a33393431f3a607ea9
DM45 <mda.tomsk@gmail.com>
2017-08-26 20:28:18 +0700

    Решение задачи

--- github_trending.py
+++ github_trending.py
@@ -1,11 +1,30 @@
+import requests
+import datetime
 
 
-def get_trending_repositories(top_size):
-    pass
+def get_current_date_minus_week():
+    current_data = datetime.date.today()
+    delta_date = current_data + datetime.timedelta(weeks=-1)
+    return delta_date
 
 
-def get_open_issues_amount(repo_owner, repo_name):
-    pass
+def get_trending_repositories(delta_date, top_size):
+    r = requests.get(
+        'https://api.github.com/search/repositories?q=created:>'
+        + '{}&page=1&per_page={}&sort=stars&order=desc'
+        .format(delta_date, top_size))
+    return r.json()
+
+
+def get_output_data_to_console(full_repositories_data):
+    repositories_data = full_repositories_data['items']
+    print('Name, url and count of open issues: ')
+    for datas in repositories_data:
+    	print(datas['full_name'], datas['html_url'], datas['open_issues'])
+
 
 if __name__ == '__main__':
-    pass
+    _top_size = input('Enter value of top size: ')
+    _delta_date = get_current_date_minus_week()
+    _trending_repositories = get_trending_repositories(_delta_date, _top_size)
+    output_data = get_output_data_to_console(_trending_repositories)
